d547c5e2622bcccf5af2a1bcb91e8a5ad331f121
enmohamedMallahi <mohaxxmall@gmail.com>
2021-06-29 17:15:51 +0100

    Adding a SignIn and SignUp Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { AuthProvider } from './contexts/AuthContext'
+import { AuthProvider } from './contexts/AuthContext';
 import Navbar from './components/Navbar';
 import PrivateRoute from './components/PrivateRoute';
 import Home from './pages/Home';
 import Reports from './pages/Reports';
 import Products from './pages/Products';
+import SignIn from './pages/SignIn';
 import './App.css';
 
 function App() {
@@ -14,9 +15,10 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path="/" exact component={Home} />
+          <PrivateRoute path="/" exact component={Home} />
           <PrivateRoute path="/reports" component={Reports} />
           <Route path="/products" component={Products} />
+          <Route path="/signin" component={SignIn} />
         </Switch>
       </Router>
     </AuthProvider>
